83967d8bbc60083216b22a2b91333dbbd536b0bb
devandscorp <artemda4@gmail.com>
2022-05-21 21:13:07 +0300

    feat: add solution

--- reverse_string.go
+++ reverse_string.go
@@ -2,5 +2,8 @@ package reverse_string
 
 func ReverseString(input string) (output string) {
 	// solution goes here
+	for _, symbol := range(input) {
+		output = string(symbol) + output
+	}
 	return output
 }
